d71a55016cfe24663719d2b32396d9558d375c68
lohith <lohith05amal@gmail.com>
2022-08-08 09:17:54 -0400

    seerver running

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Scrapper API.");
+});
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
