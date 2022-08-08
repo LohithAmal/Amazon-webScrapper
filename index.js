6b261dd09c8d379b80f18e4c46f5ee4c72139081
lohith <lohith05amal@gmail.com>
2022-08-08 09:46:28 -0400

    product search

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const request = require("request-promise");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+// scrapper api
 const apikey = "aa4c431689e3593d22325f9109bf2cd1";
 const baseUrl = `http://api.scraperapi.com?api_key=${apikey}&autoparse=true`;
 app.use(express.json());
@@ -28,6 +29,30 @@ app.get("/products/:productId/reviews", async (req, res) => {
   try {
     const response = await request(
       `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// GET product offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// GET search results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
     );
     res.json(JSON.parse(response));
   } catch (error) {
